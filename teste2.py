f1b0e53c9ea4ae87cc1e3c2a2ea857f7d71065e7
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-12 09:50:59 -0300

    Teste2 atualizado

--- teste2.py
+++
@@ -1,9 +0,0 @@
-divisores = 0
-n = int(input('Digite um número para verificarmos se ele é primo: '))
-for c in range(1, n + 1):
-    if n % c == 0:
-        divisores += 1
-if divisores == 2:
-    print('{} é primo!'.format(n))
-else:
-    print('{} não é primo!'.format(n))
