2fe41c3757d98a2b03611663d0ecb560f0b34d5b
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-13 11:33:41 -0300

    The code has been improved

--- desafio058.py
+++ desafio058.py
@@ -1,8 +1,8 @@
 from random import randrange
-pc_number = randrange(0, 11)
-user_number = 12
+pc_number = randrange(0, 101)
+user_number = 200
 count_attempt = 1
-print('From 0 to 10, can you guess the number I thought? ')
+print('From 0 to 100, can you guess the number I thought? ')
 while user_number != pc_number:
     user_number = int(input('{} attempt: '.format(count_attempt)))
     if user_number == pc_number:
@@ -10,4 +10,8 @@ while user_number != pc_number:
     else:
         print('{}? Absolutely not. Try again! '.format(user_number))
         count_attempt += 1
+        if user_number < pc_number:
+            print('Your choice was smaller than mine.')
+        else:
+            print('Your choice was bigger than mine.')
 print('It took you {} attempt(s) to guess! '.format(count_attempt))
