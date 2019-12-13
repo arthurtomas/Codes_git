e4a39f99004af7f93eb21e3c8382c25ec53058ad
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-13 11:08:15 -0300

    The code has been simplified

--- desafio057.py
+++ desafio057.py
@@ -1,7 +1,7 @@
 gender = ' '
-while gender != 'M' and gender != 'F':
+while gender not in 'MF':
     gender = str(input('Digite seu sexo [M/F]: ')).upper().strip()
-    if gender == 'M' or gender == 'F':
+    if gender in 'MF':
         print('Você é: \033[1;30m{}\033[m'.format(gender))
     else:
         print('\033[1;30m{}\033[m não é um sexo válido, por favor tente novamente.'.format(gender))
