0924e12c65788ca3769730046a8240ddfb2a3827
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-16 18:49:41 -0300

    Challenge 072 has been fixed

--- desafio072.py
+++ desafio072.py
@@ -1,11 +1,11 @@
-tupla_numbers = ('um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',
+tupla_numbers = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove',
                  'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete',
                  'dezoito', 'dezenove', 'vinte')
 while True:
     print('-=' * 27)
-    n = int(input('Digite um número entre 1 e 20: '))
-    if n < 1 or n > 20:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if n < 0 or n > 20:
         print('Número inválido, tente novamente abaixo! ')
     else:
-        print(f'O número digitado foi {tupla_numbers[n-1]}.')
+        print(f'O número digitado foi {tupla_numbers[n]}.')
         break
