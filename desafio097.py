880d849593dde8d7958e22b883758b757e323dcb
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-26 16:16:31 -0300

    Challenge 097 Completed

---
+++ desafio097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    print('~' * (len(msg)+4))
+    print(f'  {msg}  ')
+    print('~' * (len(msg)+4))
+
+
+# Programa Principal
+escreva('Parabéns')
+escreva('Tá de sacanagem?')
+escreva('Vamos ver no que isso realmente pode dar.')
+escreva("Let's see what happens!")
