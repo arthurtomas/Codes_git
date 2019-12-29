594a76167a88d9affe547a961a846623a61f8625
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-29 12:14:14 -0300

    Módulo moeda criado

---
+++ moeda.py
@@ -0,0 +1,18 @@
+def metade(num):
+    return num / 2
+
+
+def dobro(num):
+    return num * 2
+
+
+def aumento(num, porcentagem):
+    return num + (num * (porcentagem/100))
+
+
+def desconto(num, porcentagem):
+    return num - (num * (porcentagem/100))
+
+
+
+
