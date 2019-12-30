e919bc0952e12902668f481de2be23cea1a38190
arthurtomas <arthurtomas.ep@gmail.com>
2019-12-30 11:28:49 -0300

    Challenge 113 Completed

---
+++ desafio113.py
@@ -0,0 +1,7 @@
+from Barzilay.colors import printblue
+from useful import verif_int, verif_real
+
+i = verif_int()
+printblue(f'{i} é um número inteiro!')
+r = verif_real()
+printblue(f'{r} é um número real!')
